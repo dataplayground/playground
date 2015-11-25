ef622c4f7a325a0c89e0bf24b8133f02344a3e19
geoHeil <e1225063@student.tuwien.ac.at>
2015-11-25 21:10:31 +0100

    sample angular app -- working on web-socket

--- build.sbt
+++ build.sbt
@@ -10,8 +10,25 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
+
+  "org.webjars" % "bootstrap" % "3.3.5",
+  "org.webjars" % "angularjs" % "1.4.7",
+  "org.webjars" % "angular-ui-bootstrap" % "0.14.3",
+  "org.webjars.bower" % "angular-websocket" % "1.0.14",
   specs2 % Test
 )
+
+pipelineStages := Seq(uglify, digest, gzip)
+
+pipelineStages in Assets := Seq()
+
+pipelineStages := Seq(uglify, digest, gzip)
+
+DigestKeys.algorithms += "sha1"
+
+UglifyKeys.uglifyOps := { js =>
+  Seq((js.sortBy(_._2), "concat.min.js"))
+}
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
 
---
+++ public/javascripts/main.js
@@ -0,0 +1,44 @@
+var myApp = angular.module('myApp', ['ngWebSocket']);
+
+myApp.controller('MainCtrl', function ($scope) {
+
+    $scope.list = [];
+    $scope.text = 'spark';
+
+
+    $scope.filterForm = {};
+    $scope.filterForm.keyword = "spark";
+
+    $scope.submit = function () {
+        if ($scope.filterForm.keyword) {
+            $scope.list.push(this.filterForm.keyword);
+            $scope.filterForm.keyword = '';
+        }
+    };
+
+
+});
+
+myApp.factory('MyData', function ($websocket) {
+        // Open a WebSocket connection
+        var dataStream = $websocket('wss://localhost:9000/socket');
+
+        var collection = [];
+
+        dataStream.onMessage(function (message) {
+            collection.push(JSON.parse(message.data));
+        });
+
+        var methods = {
+            collection: collection,
+            get: function () {
+                dataStream.send(JSON.stringify({action: 'get'}));
+            }
+        };
+
+        return methods;
+    })
+    .controller('SomeController', function ($scope, MyData) {
+        $scope.MyData = MyData;
+        $scope.testData = "asdf"
+    });
