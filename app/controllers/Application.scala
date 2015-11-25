c5f81f38dd0decec57e65ee46538f37538c33496
geoHeil <e1225063@student.tuwien.ac.at>
2015-11-25 13:28:27 +0100

    @singleton error

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,33 @@
 package controllers
 
-import play.api._
+import javax.inject.Inject
+
+import actors.HelloActor
+import actors.HelloActor.SayHello
+import akka.actor.ActorSystem
 import play.api.mvc._
 
-class Application extends Controller {
+//using plays own actor system
+@Singleton
+class Application @Inject()(system: ActorSystem) extends Controller {
+
+  val helloActor = system.actorOf(HelloActor.props, "hello-actor")
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  import akka.pattern.ask
+  import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+  import scala.concurrent.duration._
+
+  implicit val timeout = 5.seconds
+
+  def sayHello(name: String) = Action.async {
+    (helloActor ? SayHello(name)).mapTo[String].map { message =>
+      Ok(message)
+    }
   }
 
 }
