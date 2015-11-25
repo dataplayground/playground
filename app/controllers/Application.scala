f8f766413a0330515ea5485dc4b147a290e05f93
geoHeil <e1225063@student.tuwien.ac.at>
2015-11-25 13:55:32 +0100

    adding a simple web socket backend

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,33 +1,37 @@
 package controllers
 
-import javax.inject.{Singleton, Inject}
+import javax.inject.{Inject, Singleton}
 
-import actors.HelloActor
 import actors.HelloActor.SayHello
+import actors.{HelloActor, MyWebSocketActor}
 import akka.actor.ActorSystem
+import akka.pattern.ask
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
+
+import scala.concurrent.duration._
+
 
 //using plays own actor system
 @Singleton
 class Application @Inject()(system: ActorSystem) extends Controller {
 
   val helloActor = system.actorOf(HelloActor.props, "hello-actor")
+  implicit val timeout = akka.util.Timeout(5.seconds)
+  implicit val app = play.api.Play.current
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  import akka.pattern.ask
-  import play.api.libs.concurrent.Execution.Implicits.defaultContext
-
-  import scala.concurrent.duration._
-
-  implicit val timeout = akka.util.Timeout(5.seconds)
-
   def sayHello(name: String) = Action.async {
     (helloActor ? SayHello(name)).mapTo[String].map { message =>
       Ok(message)
     }
+  }
+
+  def socket = WebSocket.acceptWithActor[String, String] { request => out =>
+    MyWebSocketActor.props(out)
   }
 
 }
