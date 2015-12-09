0ac9d2ecd8a5980f574e76b3662caa204ad5eecb
geoHeil <e1225063@student.tuwien.ac.at>
2015-12-09 10:27:23 +0100

    dynamic echo ;)

--- app/actors/MySocketActor.scala
+++ app/actors/MySocketActor.scala
@@ -2,7 +2,7 @@ package actors
 
 import akka.actor.{ Actor, ActorRef, Props }
 import play.api.Logger
-import play.api.libs.json.{ JsValue, Json }
+import play.api.libs.json.{ JsObject, JsValue, Json }
 
 object MyWebSocketActor {
   def props(out: ActorRef) = Props(new MyWebSocketActor(out))
@@ -14,12 +14,18 @@ class MyWebSocketActor(out: ActorRef) extends Actor {
       Logger.debug("actor something  " + out)
       Logger.debug("message  " + Json.prettyPrint(msg))
 
-      val value = """
-      {
-          "fooReply" : "$message"
-      }
-                  """
-      val json: JsValue = Json.parse(value)
+      //      val value =
+      //        """
+      //      {
+      //          "fooReply" : "$message"
+      //      }
+      //        """
+
+      val msgVal = msg \ "foo"
+      val json: JsValue = JsObject(Seq("message" -> msgVal.get))
+
+      //      val jValue = new JsString("")
+      //      val json: JsValue = Json.parse(value)
       out ! (json)
   }
 }
